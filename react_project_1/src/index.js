8a167b81d512cefb999a16108335d698e1e82f11
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-21 18:35:48 -0700

    Initialized React App and all necessary folders

---
+++ react_project_1/src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function MyApp() {
+	return (
+	<ul>HELLO</ul>
+	)
+}
+
+ReactDOM.render(
+	<MyApp/>,
+	document.getElementById("root")
+	)
+
+
