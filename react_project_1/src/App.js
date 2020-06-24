6d82266d90021746bd5d7eb08e4c8da939a679fe
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-23 17:59:37 -0700

    Created Js in JSX to wish the user correct time of day

--- react_project_1/src/App.js
+++ react_project_1/src/App.js
@@ -1,38 +1,23 @@
+
 import React from "react"
-import Footer from "./footer"
+
 
 function App() {
-  return (
-    <div>
-    <h1> APPy</h1>
-    <Footer />
-    </div>
-    )
+  const date = new Date()
+  const hours = date.gethours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  }
+  else if (hours > 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  }
+  else {
+    timeOfDay = "night"
+  }
+ 
+ return (
+  <h1> Good {timeOfDay}! </h1>
+  )
 }
-
-
-export default App 
-
-
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- react_project_1/src/index.js
+++ react_project_1/src/index.js
@@ -2,6 +2,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import App from "./App.js"
+function App() {  
+   const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  }
+  else if (hours > 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  }
+  else {
+    timeOfDay = "night"
+  }
+ 
+ return (
+  <h1> Good {timeOfDay}! </h1>
+  )
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"))
