a3a0b1f5fe734dbfeb566ecbf848c9d665507908
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-01 21:27:07 -0700

    Added components to repository

---
+++ react_todo/src/components/ToDoItem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function ToDoItem(props) {
+	return (
+		<div className="todo">
+			<input type="checkbox" 
+			 checked={props.item.completed} 
+			 onChange={() => props.handleChange(props.item.id)}/>
+			<p> {props.item.text} </p>
+		</div>
+		)
+}
+
+export default ToDoItem
