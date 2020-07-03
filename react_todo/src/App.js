ec90b37dcd177495eba6af345e39c95c8b59dd87
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-03 11:30:33 -0700

    Created button for log in and log out and display text

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -120,12 +120,33 @@ import todosData from "./todosData"
 
 // Conditional Rendering practice
 
-function App() {
+class App extends React.Component {
+	constructor () {
+	super()
+	this.state = {
+		isLoggedIn: false
+	}
+	this.handleClick = this.handleClick.bind(this)
+}
+
+handleClick() {
+	this.setState(prevState => {
+		return {
+			isLoggedIn: !prevState.isLoggedIn
+		}
+	})
+}
+	render() {
+		let buttonText = this.state.isLoggedIn ? "Log out" : "Log in"
+		let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
 	return (
 		<div>
+		<button onClick= {this.handleClick> {buttonText}</button>
+		{<h1> {displayText} </h1>
 		</div>
 
 	)
+}
 }
 
 export default App
