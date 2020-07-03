975b8837512b020aca210c6e7b4c38a070c32472
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-03 14:21:50 -0700

    Setting up for forms practice

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -120,33 +120,53 @@ import todosData from "./todosData"
 
 // Conditional Rendering practice
 
-class App extends React.Component {
-	constructor () {
-	super()
-	this.state = {
-		isLoggedIn: false
-	}
-	this.handleClick = this.handleClick.bind(this)
-}
+// class App extends React.Component {
+// 	constructor () {
+// 	super()
+// 	this.state = {
+// 		isLoggedIn: false
+// 	}
+// 	this.handleClick = this.handleClick.bind(this)
+// }
 
-handleClick() {
-	this.setState(prevState => {
-		return {
-			isLoggedIn: !prevState.isLoggedIn
+// handleClick() {
+// 	this.setState(prevState => {
+// 		return {
+// 			isLoggedIn: !prevState.isLoggedIn
+// 		}
+// 	})
+// }
+// 	render() {
+// 		let buttonText = this.state.isLoggedIn ? "Log out" : "Log in"
+// 		let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
+// 	return (
+// 		<div>
+// 		<button onClick= {this.handleClick> {buttonText}</button>
+// 		{<h1> {displayText} </h1>
+// 		</div>
+
+// 	)
+// }
+// }
+
+// export default App
+
+// Forms practice
+
+import React, {Component} from "react"
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+
 		}
-	})
-}
-	render() {
-		let buttonText = this.state.isLoggedIn ? "Log out" : "Log in"
-		let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
-	return (
-		<div>
-		<button onClick= {this.handleClick> {buttonText}</button>
-		{<h1> {displayText} </h1>
-		</div>
 
-	)
+		render() {
+			return (
+				<div>
+				</div>
+				)
+		}
+	}
 }
-}
-
-export default App
